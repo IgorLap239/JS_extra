478a8a9913f0211c9f9f94e3ebc1922a2655b8e6
Igor <ilaptev@ya.ru>
2021-02-02 22:24:12 +0300

    Дополнительное задание к 4 уроку

--- js/script.js
+++ js/script.js
@@ -1,56 +1,19 @@
 'use strict';
 
-let num = 266219;
-let digits = num.toString().split('');
-let realDigits = digits.map(Number);
-function arrayProduct(array) {
-    let product = 1;
-    for (var i = 0; i < array.length; i++) {
-        product *= array[i];
-        }
-    return product;
-}
+let test = prompt('Введите тестовый аргумент')
 
-let arrayProductResult = arrayProduct(realDigits);
+let extraLesson4Function = function (param) {
+    if (typeof(param) !== 'string') {
+        alert('Введенный аргумент должен быть строкой. Повторите ввод');
+        test = prompt('На этот раз введите строку)');
+    }
 
-console.log(arrayProductResult);
+    if (param.length <= 30) {
+        return param.trim();
+    } else {
+        param.trim();
+        return (param.substr(0, 29) + '...');
+    }   
+};
 
-let extent = arrayProductResult ** 3;
-
-let extentDigits = extent.toString().split('');
-
-console.log('Первые две цифры результата: ' + extentDigits[0]+ ', ' + extentDigits[1]);
-
-//дополнительное задание к третьему уроку
-
-//первая задача 
-
-//узнать значение атрибута lang тега html
-let lang = document.getElementsByTagName("html")[0].getAttribute("lang");
-
-//вывести названия дней недели на соответствующем языке
-
-//способо с if
-if (lang === 'ru') {
-    console.log('Пн, Вт, Ср, Чт, Пт, Сб, Вс');
-} else {
-    console.log('Mon, Tues, Wed, Thurs, Fri, Sat, Sun');
-} 
-
-//способ  switch
-switch (lang) {
-    case "en":
-        console.log('Mon, Tues, Wed, Thurs, Fri, Sat, Sun');
-        break;
-    default: 
-        console.log('Пн, Вт, Ср, Чт, Пт, Сб, Вс');
-}
-
-//тернарный оператор
-lang === "en" ? console.log('Mon, Tues, Wed, Thurs, Fri, Sat, Sun') :
-     console.log('Пн, Вт, Ср, Чт, Пт, Сб, Вс');
-
-//вторая задача
-let namePerson = prompt("Введите имя");
-namePerson === "Артем" ? console.log("Директор") :
-    namePerson === "Максим" ? console.log("Преподаватель") : console.log("Студент");
+console.log(extraLesson4Function(test));
