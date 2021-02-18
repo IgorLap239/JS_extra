aa5218a01f1a6624d603a6ad3bf5f23f1f98bdca
Igor <ilaptev@ya.ru>
2021-02-19 00:02:06 +0300

    Дополнительное задание к 9 уроку

--- js/script.js
+++ js/script.js
@@ -1,32 +1,46 @@
 'use strict';
 
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресение'];
+const variantA = () => {
+    let a = document.getElementById('a');
+    let date = new Date();
+    let week = ['Воскресение', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    let month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+    let strHours;
+    let strMinutes;
+    let strSeconds;
 
-let date = new Date();
-
-let outWeek = document.getElementById('out_week');
-let str = ' ';
-
-let getEuDay = function () {
-    let y;
-    if (date.getDay() >= 1) {
-        y = date.getDay() - 1;
-    } else if (date.getDay() === 0) {
-        y = 6;
+    if (date.getHours() === 1 || date.getHours() === 21) {
+        strHours = 'час';
+    } else if (2 <= date.getHours() < 5  || 22 <= date.getHours()) {
+        strHours = 'часa';
+    } else {
+        strHours = 'часов';
     }
-    return y;
+
+    if (date.getMinutes().toString().slice(-1) === 1) {
+        strMinutes = 'минута';
+    } else if (2 <= date.getMinutes().toString().slice(-1) && date.getMinutes().toString().slice(-1) < 5) {
+        strMinutes = 'минуты';
+    } else {
+        strMinutes = 'минут';
+    }
+
+    if (date.getSeconds().toString().slice(-1) === 1) {
+        strSeconds = 'секунда';
+    } else if (2 <= date.getSeconds().toString().slice(-1) && date.getSeconds().toString().slice(-1) < 5) {
+        strSeconds = 'секунды';
+    } else {
+        strSeconds = 'секунд';
+    }
+
+    a.innerHTML = `Сегодня ${week[date.getDay()]}, ${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()} года, ${date.getHours()} ${strHours} ${date.getMinutes()} ${strMinutes} ${date.getSeconds()} ${strSeconds}.`; 
 };
 
-for (let i = 0; i < week.length; i++) {
-    if ( i !== 5 && i !== 6 && i !== getEuDay() ) {
-        str += week[i] + '<br>';
-    } else if ( i !== getEuDay() && (i === 5 || i === 6) ) {
-        str += '<i>' + week[i] + '</i>' + '<br>';
-    } else if ( i === getEuDay() && i !== 5 && i !== 6 ) {
-        str += '<b>' + week[i] + '</b>' + '<br>';
-    } else if ( i === getEuDay() && (i === 5 || i === 6) ) {
-        str += '<b>' + '<i>' + week[i] + '</i>' + '</b>' + '<br>';
-    }
-}
+const variantB = () => {
+    let b = document.getElementById('b');
+    let date = new Date();
+    b.innerHTML = date.toLocaleString();
+};
 
-outWeek.innerHTML = str;
+setInterval(variantA, 1000); 
+setInterval(variantB, 1000);
