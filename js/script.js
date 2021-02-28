0932e201860f0c359b5cf6fe05759ac95b6cc467
Igor <ilaptev@ya.ru>
2021-02-28 10:18:23 +0300

    Дополнительное задание к 13 уроку. Рандомный фон

--- js/script.js
+++ js/script.js
@@ -1,46 +1,25 @@
 'use strict';
 
-const variantA = () => {
-    let a = document.getElementById('a');
-    let date = new Date();
-    let week = ['Воскресение', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-    let month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
-    let strHours;
-    let strMinutes;
-    let strSeconds;
+const button = document.querySelector('#change'),
+    text = document.querySelector('#color'),
+    body = document.querySelector('body');
 
-    if (date.getHours() === 1 || date.getHours() === 21) {
-        strHours = 'час';
-    } else if (2 <= date.getHours() < 5  || 22 <= date.getHours()) {
-        strHours = 'часa';
-    } else {
-        strHours = 'часов';
+
+body.style.backgroundColor = '#008d3b';
+
+const generateRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
-
-    if (date.getMinutes().toString().slice(-1) === 1) {
-        strMinutes = 'минута';
-    } else if (2 <= date.getMinutes().toString().slice(-1) && date.getMinutes().toString().slice(-1) < 5) {
-        strMinutes = 'минуты';
-    } else {
-        strMinutes = 'минут';
-    }
-
-    if (date.getSeconds().toString().slice(-1) === 1) {
-        strSeconds = 'секунда';
-    } else if (2 <= date.getSeconds().toString().slice(-1) && date.getSeconds().toString().slice(-1) < 5) {
-        strSeconds = 'секунды';
-    } else {
-        strSeconds = 'секунд';
-    }
-
-    a.innerHTML = `Сегодня ${week[date.getDay()]}, ${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()} года, ${date.getHours()} ${strHours} ${date.getMinutes()} ${strMinutes} ${date.getSeconds()} ${strSeconds}.`; 
+    return color;
 };
 
-const variantB = () => {
-    let b = document.getElementById('b');
-    let date = new Date();
-    b.innerHTML = date.toLocaleString();
+let changeColor = () => {
+    let color = generateRandomColor();
+    body.style.backgroundColor = color;
+    text.textContent = color;
 };
 
-setInterval(variantA, 1000); 
-setInterval(variantB, 1000);
+button.addEventListener('click', changeColor);
